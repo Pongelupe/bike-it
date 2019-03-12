d33c8f008264d8bbb0015a5d15b4458776607bd8
Pongelupe <pongelupe1998@gmail.com>
2019-03-12 00:50:13 -0300

    added model Segment

--- src/main/java/br/com/pongelupe/bikeit/App.java
+++ src/main/java/br/com/pongelupe/bikeit/App.java
@@ -5,9 +5,10 @@ import br.com.pongelupe.bikeit.services.impl.StravaService;
 
 public class App {
 
-	public static void main(String[] args) {
+	public static void main(String... args) {
 		StravaService stravaService = new StravaService();
 		Auth oauthToken = stravaService.getOauthToken();
 		System.out.println(oauthToken);
+		System.exit(0);
 	}
 }
---
+++ src/main/java/br/com/pongelupe/bikeit/model/Segment.java
@@ -0,0 +1,12 @@
+package br.com.pongelupe.bikeit.model;
+
+import lombok.Data;
+
+@Data
+public class Segment {
+
+	private int id;
+	private String name;
+	private double distance;
+	private String points;
+}
